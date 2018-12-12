a9f25385c64560e782395f31243a93dba0c00963
Estuardo Ramos <estuardoramos.ec10@gmail.com>
2018-12-11 19:23:36 -0600

    resolviendo el puzzle 2 del día 3: The claim that does not repeat space is: 567

--- day3/src/main/cpp/main.cpp
+++ day3/src/main/cpp/main.cpp
@@ -47,26 +47,49 @@ int total_square_inches(int (&data)[1000][1000]) {
    return foo;
 }
 
-void first_puzzle(ifstream &file) {
-   int i, j;
+int analyze_data(ifstream& file, int (&data)[1000][1000], bool rereading) {
    string line;
-   int chimney_squeeze[1000][1000];
-   initialize_matix(chimney_squeeze, 0);
+   int i, j, foo = 0;
    if (file.is_open()) {
       while (getline(file, line)) {
+         bool one_use = true;
          vector<string> args_line = string_split(line, " ");
          vector<string> coordinates = string_split(args_line[2], ",");
          vector<string> size = string_split(args_line[3], "x");
          coordinates[1].erase(coordinates[1].find(":"));
          for (i = stoi(coordinates[0]) +1; i <= stoi(coordinates[0]) + stoi(size[0]); i++) {
             for (j = stoi(coordinates[1]) +1; j <= stoi(coordinates[1]) + stoi(size[1]); j++) {
-               chimney_squeeze[i][j]++;
+               if (rereading && data[i][j] > 1) {
+                  one_use = false;
+               } else if (!rereading) {
+                  data[i][j]++;
+               }
             }
+         }
+         if (one_use) {
+            foo = stoi(args_line[0].erase(0, 1));
          }
       }
    }
+   return foo;
+}
+
+void first_puzzle(ifstream &file) {
+   int chimney_squeeze[1000][1000];
+   initialize_matix(chimney_squeeze, 0);
+   analyze_data(file, chimney_squeeze, false);
    int square_inches = total_square_inches(chimney_squeeze);
    printf("Square inches reused is: %d\n", square_inches);
+}
+
+void second_puzzle(ifstream &file) {
+   int chimney_squeeze[1000][1000];
+   initialize_matix(chimney_squeeze, 0);
+   analyze_data(file, chimney_squeeze, false);
+   file.clear();
+   file.seekg(0, file.beg);
+   int req = analyze_data(file, chimney_squeeze, true);
+   printf("The claim that does not repeat space is: %d\n", req);
 }
 
 int main(int argc, char** argv) {
@@ -74,6 +97,8 @@ int main(int argc, char** argv) {
    string option(argv[1]);
    if (option == "1") {
       first_puzzle(file);
+   } else if (option == "2") {
+      second_puzzle(file);
    }
    file.close();
    return 0;
